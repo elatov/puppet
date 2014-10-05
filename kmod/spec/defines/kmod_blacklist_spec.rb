fa79296ea85e4c480b954e59b087d8c27aa8bdfa
Karim Elatov <elatov@gmail.com>
2014-10-05 17:24:09 -0600

    add kmod

---
+++ kmod/spec/defines/kmod_blacklist_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe 'kmod::blacklist', :type => :define do
+  let(:title) { 'foo' }
+  let(:facts) do { :osfamily => 'Debian', } end
+  context 'when ensure is set to present' do
+    let(:params) do { :ensure => 'present', :file => '/bar/baz' } end
+    it { should contain_kmod__blacklist('foo') }
+    it { should contain_kmod__generic('blacklist foo').with({
+      'ensure' => 'present',
+      'type'   => 'blacklist',
+      'module' => 'foo',
+      'file'   => '/bar/baz'
+    }) }
+  end
+
+  context 'when ensure is set to absent' do
+    let(:params) do { :ensure => 'absent', :file => '/bar/baz' } end
+    it { should contain_kmod__blacklist('foo') }
+    it { should contain_kmod__generic('blacklist foo').with({
+      'ensure' => 'absent',
+      'type'   => 'blacklist',
+      'module' => 'foo',
+      'file'   => '/bar/baz'
+    }) }
+  end
+end
