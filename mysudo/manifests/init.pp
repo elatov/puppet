7e99e7f29bb27c8e17e4f12c4208e23444c375f6
Karim Elatov <elatov@gmail.com>
2014-10-11 11:55:08 -0600

    add mysudo

---
+++ mysudo/manifests/init.pp
@@ -0,0 +1,43 @@
+# == Class: mysudo
+#
+# Full description of class mysudo here.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#
+class mysudo {
+
+	class { 'sudo': 
+	 config_file_replace => false,
+	}
+  	
+  case $::osfamily {
+    'Debian': {
+      sudo::conf { 'admins':
+        priority => 10,
+        content  => "%adm ALL=(ALL) ALL",
+      }
+    }
+    'RedHat': {
+			sudo::conf { 'admins':
+				priority => 10,
+				content  => "%wheel ALL=(ALL) ALL",
+			}
+			sudo::conf { 'elatov':
+				priority => 20,
+				content  => "Defaults:elatov secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin",
+			}
+    }
+    'FreeBSD': {
+      sudo::conf { 'admins':
+        priority => 10,
+        content  => "%wheel ALL=(ALL) ALL",
+      }
+    }
+    default: {
+      fail("${::operatingsystem} not supported")
+    }
+  }
+}
