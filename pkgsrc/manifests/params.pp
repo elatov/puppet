62849697a5f026ae8989bbb906e9cdf7612d3d24
Karim Elatov <elatov@gmail.com>
2014-10-18 13:33:11 -0600

    f

--- pkgsrc/manifests/params.pp
+++ pkgsrc/manifests/params.pp
@@ -5,18 +5,16 @@
 #
 class pkgsrc::params {
 
-	$pkgsrc_settings_all	=	{	'version' 	=> '2014Q2',
+	$pkgsrc_default_settings	=	{	'version' 	=> '2014Q2',
 	                              'url'       => 'pkgsrc.joyent.com/packages/SmartOS/bootstrap',
                                 'upgrade'   => false,
 										          }
 	case $::osfamily {
 		'Solaris': {
 			$pkgsrc_home						= '/opt/local'
-			$pkgsrc_settings_os		= { }
 		}
 		default: {
 			fail("${::operatingsystem} not supported")
 		}
 	}
-	$pkgsrc_default_settings = merge($pkgsrc_settings_all,$pkgsrc_settings_os)
 }
