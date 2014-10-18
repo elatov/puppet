5157cf129c4f6f0c6422d447db7923f7cf0b1f0d
Karim Elatov <elatov@gmail.com>
2014-10-18 14:57:49 -0600

    sf

--- pkgsrc/manifests/config.pp
+++ pkgsrc/manifests/config.pp
@@ -17,20 +17,20 @@ class pkgsrc::config {
     path        => ['bin','/usr/bin'],
     cwd         => '/root/apps',
     command     => "tar xzf /root/apps/bootstrap-${pkgsrc::settings['version']}-x86_64.tar.gz -C /",
-    require     => File[$pkgsrc::settings['home']],
+    require     => File[$pkgsrc::home],
     refreshonly => true,
     creates     => "/opt/local/etc"
  }~>
  exec { "${module_name}-rebuild-pkgsrc_index":
     path        => ['/opt/local/sbin','/opt/local/bin'],
     command     => "pkg_admin rebuild",
-    require     => File[$pkgsrc::settings['home']],
+    require     => File[$pkgsrc::home],
     refreshonly => true,
  }~>
  exec { "${module_name}-update-pkgsrc_db":
     path        => ['/opt/local/sbin','/opt/local/bin'],
     command     => "pkgin -y up",
-    require     => File[$pkgsrc::settings['home']],
+    require     => File[$pkgsrc::home],
     refreshonly => true,
  }
 }
