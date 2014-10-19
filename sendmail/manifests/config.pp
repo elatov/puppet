a168f443b7c75272401452d3beb8717afa61864d
Karim Elatov <elatov@gmail.com>
2014-10-19 10:28:52 -0600

    more sendmail config

--- sendmail/manifests/config.pp
+++ sendmail/manifests/config.pp
@@ -13,5 +13,10 @@ class sendmail::config {
     path    => "${sendmail::cf_dir}/${sendmail::mc_file}",
     content => template("sendmail/${sendmail::mc_file}.erb"),
     require => File [$sendmail::cf_dir],
+  }->
+  exec { "${module_name}-m4-gen-sm-config":
+    cwd     => $sendmail::cf_dir,
+    command => "/usr/ccs/bin/m4 ../m4/cf.m4 sm.mc > sm.cf",
+    creates => "${sendmail::cf_dir}/sm.cf",
   }
 }
