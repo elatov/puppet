156d5c9aa275cf88ad4ff277eed70adb5f7a54d7
Karim Elatov <elatov@gmail.com>
2014-10-19 11:58:37 -0600

    re test

--- rsyslog/manifests/install.pp
+++ rsyslog/manifests/install.pp
@@ -1,5 +1,5 @@
 class rsyslog::install {
-  if ($::osfamily != /(?i:FreeBSD|Solaris)/ ){
+  if $::osfamily !~ /(?i:FreeBSD|Solaris)/ {
     ensure_packages($rsyslog::package_name,{ensure => 'present'})
   }
 }
