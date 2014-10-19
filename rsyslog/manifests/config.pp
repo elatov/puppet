f3d397aa7e316c170951e4eeeb0301dd710bd78b
Karim Elatov <elatov@gmail.com>
2014-10-19 12:18:38 -0600

    sdf

--- rsyslog/manifests/config.pp
+++ rsyslog/manifests/config.pp
@@ -47,10 +47,7 @@ class rsyslog::config {
   }
   
   if ($::osfamily == 'Solaris'){
-    file_line { "enable_remote_in_${rsyslog::conf_file}":
-      path => $rsyslog::conf_file,
-      line => "*.emerg;*.alert;*.crit;*.err;*.warning;*.notice;*.info;*.debug;\t@${rsyslog::settings['server']}",
-    }
+   
     file { '/var/adm/mail.log':
       ensure => 'present',
     }->
@@ -58,19 +55,21 @@ class rsyslog::config {
       path  => $rsyslog::conf_file,
       match => "^mail.debug*",
       line  => "mail.debug;mail.err;mail.alert;mail.warning;mail.info;mail.notice;mail.emerg;mail.crit;\t/var/adm/mail.log",
-    } 
-  
+    }->
     file_line { "enable_smartd_in_${rsyslog::conf_file}":
       path => $rsyslog::conf_file,
       line => "local3.warning;local3.err;local3.alert;local3.debug;local3.info;local3.notice;local3.debug;local3.crit;\t/var/adm/smartd.log",
-    } 
-    
-     file { '/var/adm/auth.log':
+    }-> 
+    file { '/var/adm/auth.log':
       ensure => 'present',
     }->
     file_line { "enable_auth_in_${rsyslog::conf_file}":
       path => $rsyslog::conf_file,
       line => "auth.warning;auth.err;auth.alert;auth.debug;auth.info;auth.notice;auth.debug;auth.crit;\t/var/adm/auth.log",
-    } 
+    }-> 
+    file_line { "enable_remote_in_${rsyslog::conf_file}":
+      path => $rsyslog::conf_file,
+      line => "*.emerg;*.alert;*.crit;*.err;*.warning;*.notice;*.info;*.debug;\t@${rsyslog::settings['server']}",
+    }
   }
 }
