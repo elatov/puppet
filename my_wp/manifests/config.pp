aab20bdbe76ac3559e6eeaaf4b76f6a9403b5321
Karim Elatov <elatov@gmail.com>
2014-11-27 12:07:49 -0700

    update template config

--- my_wp/manifests/config.pp
+++ my_wp/manifests/config.pp
@@ -5,7 +5,6 @@
 class my_wp::config {
 
   if ($my_wp::settings['enable_main_wp']){
-	  
     file { "${my_wp::apache_confdir}/${my_wp::settings['main_wp_name']}.conf":
 	    ensure  => 'present',
 	    content  => template("my_wp/apache.conf.erb"),
@@ -13,10 +12,9 @@ class my_wp::config {
   }
   
   if ($my_wp::settings['enable_cs_wp']){
-    
     file { "${my_wp::apache_confdir}/${my_wp::settings['cs_wp_name']}.conf":
       ensure  => 'present',
-      content  => template("my_wp/apache.conf.erb"),
+      content  => template("my_wp/cs_apache.conf.erb"),
     }
 	}
 }
