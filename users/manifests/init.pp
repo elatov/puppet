d4bb9868cee72ef15fc4a39a1b965e4ca0f8bdd3
Karim Elatov <elatov@gmail.com>
2015-12-13 13:49:46 -0700

    update redhat to use zsh

--- users/manifests/init.pp
+++ users/manifests/init.pp
@@ -24,7 +24,7 @@ class users {
 				gid         => "1000",
 				groups      => ["wheel"],
 				membership  => minimum,
-				shell       => "/bin/bash",
+				shell       => "/bin/zsh",
 				allowdupe   => false,
 				managehome  => true,
 				require     => Group["elatov"]
