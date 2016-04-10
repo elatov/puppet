46d06d963020488053a9163afaf39d6efe023526
elatov <elatov@users.noreply.github.com>
2016-04-10 11:33:53 -0600

    sdf

--- my_mysql/manifests/install.pp
+++ my_mysql/manifests/install.pp
@@ -2,16 +2,13 @@
 #
 class my_mysql::install {
 
-  Package <| title == 'mysql-server' |> {
-      name => 'mariadb-server',
-    }
-    Package <| title == 'mysql_client' |> {
-      name => 'mariadb-client',
-    }
 	class { '::mysql::server':
 		root_password           => $my_mysql::settings['root_password'],
 		manage_config_file      => false,
 		remove_default_accounts => true,
 		package_name            => $my_mysql::package_name,
 	}
+	class { '::mysql::client':
+    package_name            => 'mariadb-client',
+  }
 }
