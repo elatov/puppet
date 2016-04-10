bfe8d498a27ebdb4dcdacac5c02cfaa2cc8ede56
elatov <elatov@users.noreply.github.com>
2016-04-10 11:22:11 -0600

    keep on chugging

--- my_mysql/manifests/install.pp
+++ my_mysql/manifests/install.pp
@@ -2,6 +2,12 @@
 #
 class my_mysql::install {
 
+  Package <| title == 'mysql-server' |> {
+      name => 'mariadb-server',
+    }
+    Package <| title == 'mysql_client' |> {
+      name => 'mariadb-client',
+    }
 	class { '::mysql::server':
 		root_password           => $my_mysql::settings['root_password'],
 		manage_config_file      => false,
