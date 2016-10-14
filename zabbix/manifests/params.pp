c646dff4093d6c1ba5048a810efc3e27fa1ca83b
Karim Elatov <elatov@users.noreply.github.com>
2016-10-14 10:47:35 -0600

    we'll get there

--- zabbix/manifests/params.pp
+++ zabbix/manifests/params.pp
@@ -50,11 +50,8 @@ class zabbix::params {
 			$zabbix_server_service_name        = 'zabbix-server'
 			
 			### Client/Agent
-			$agent_zabbix_config_dir             = '/usr/local/etc/zabbix3'
-			$agent_zabbix_custom_scripts_dir     = "${agent_zabbix_config_dir}/custom-scripts.d"
-			$agent_zabbix_custom_conf_dir        = "${agent_zabbix_config_dir}/zabbix_agentd.conf.d"
+
 			$agent_zabbix_config_file            = 'zabbix_agentd.conf'
-			$agent_zabbix_package_name           = 'zabbix3-agent'
 			$agent_zabbix_service_name           = 'zabbix_agentd'
 #			$agent_zabbix_version                = '3.0'
 			$agent_zabbix_default_settings       = { 'logFile'                => '/var/log/zabbix/zabbix_agentd.log',
@@ -68,6 +65,9 @@ class zabbix::params {
 			                                         'disk_perf'              => false,
 			                                         'version'                => '3',
 			                                       }
+      $agent_zabbix_config_dir             = "/usr/local/etc/zabbix${agent_zabbix_default_settings['version']}"
+      $agent_zabbix_custom_conf_dir        = "${agent_zabbix_config_dir}/zabbix_agentd.conf.d"
+      $agent_zabbix_package_name           = "zabbix${agent_zabbix_default_settings['version']}-agent"
 				
 		}
 		'Redhat': {
