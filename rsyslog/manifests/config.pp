5811f3d767cd5b934de0319af7962a445fc9a75f
Karim Elatov <elatov@users.noreply.github.com>
2016-12-13 11:26:02 -0700

    update solaris syslog to include the error priority

--- rsyslog/manifests/config.pp
+++ rsyslog/manifests/config.pp
@@ -56,7 +56,7 @@ class rsyslog::config {
     file_line { "enable_mail_in_${rsyslog::conf_file}":
       path  => $rsyslog::conf_file,
       match => "mail.debug",
-      line  => "mail.warning;mail.err;mail.alert;mail.debug;mail.info;mail.notice;mail.debug\t/var/adm/mail.log",
+      line  => "mail.warning;mail.err;mail.error;mail.alert;mail.debug;mail.info;mail.notice;mail.debug\t/var/adm/mail.log",
     }->
     file_line { "enable_mail-crit_in_${rsyslog::conf_file}":
       path  => $rsyslog::conf_file,
@@ -68,18 +68,18 @@ class rsyslog::config {
     }->
     file_line { "enable_smartd_in_${rsyslog::conf_file}":
       path => $rsyslog::conf_file,
-      line => "local3.warning;local3.err;local3.alert;local3.debug;local3.info;local3.notice;local3.debug\t/var/adm/smartd.log",
+      line => "local3.warning;local3.err;local3.error;local3.alert;local3.debug;local3.info;local3.notice;local3.debug\t/var/adm/smartd.log",
     }-> 
     file { '/var/adm/auth.log':
       ensure => 'present',
     }->
     file_line { "enable_auth_in_${rsyslog::conf_file}":
       path => $rsyslog::conf_file,
-      line => "auth.warning;auth.err;auth.alert;auth.debug;auth.info;auth.notice;auth.debug\t/var/adm/auth.log",
+      line => "auth.warning;auth.err;auth.error;auth.alert;auth.debug;auth.info;auth.notice;auth.debug\t/var/adm/auth.log",
     }-> 
     file_line { "enable_remote_in_${rsyslog::conf_file}":
       path => $rsyslog::conf_file,
-      line => "*.emerg;*.alert;*.crit;*.err;*.warning;*.notice;*.info;*.debug\t@${rsyslog::settings['server']}",
+      line => "*.emerg;*.alert;*.crit;*.err;*.error;*.warning;*.notice;*.info;*.debug\t@${rsyslog::settings['server']}",
     }~>
     exec { "${module_name}-reread-syslog-conf":
       path        => ["/bin","/usr/bin"],
