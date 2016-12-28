68d1dc4abcd886ae9bccfb95e384ee9d668fefac
Karim Elatov <elatov@users.noreply.github.com>
2016-12-28 14:53:43 -0700

    start test SSH-7408, check if hash loop is working

--- lynis/manifests/config.pp
+++ lynis/manifests/config.pp
@@ -10,6 +10,7 @@ class lynis::config {
       $service_name = 'lynis'
     }
     'RedHat': {
+      
       if ( $::lynis::settings['cron_enabled'] == true ){
         ensure_packages('crontabs',{ensure => 'present'})
 
@@ -21,12 +22,14 @@ class lynis::config {
 					links   => 'follow',
 				}
       }
+      
       if ( $::lynis::settings['tests']['AUTH-9328'] == true ){
         file { "/etc/profile.d/umask.sh":
           source  => 'puppet:///modules/lynis/umask.sh',
           mode    => '0644'
         }
       }
+      
       if ( $::lynis::settings['tests']['FILE-6310'] == true ){
         service { 'tmp.mount':
 			    ensure     => running,
@@ -34,16 +37,28 @@ class lynis::config {
 			    hasstatus  => true,
 			  }
       }
+      
       if ( $::lynis::settings['tests']['STRG-1840'] == true ){
         file { "/etc/modprobe.d/usb.conf":
           source  => 'puppet:///modules/lynis/modprobe-usb.conf',
           mode    => '0644'
         }
       }
+      
       if ( $::lynis::settings['tests']['STRG-1846'] == true ){
         file { "/etc/modprobe.d/firewire.conf":
           source  => 'puppet:///modules/lynis/modprobe-firewire.conf',
           mode    => '0644'
+        }
+      }
+      
+      if ( $::lynis::settings['tests']['NETW-3032'] == true ){
+        class {'arpwatch': }
+      }
+      
+      if ( $::lynis::settings['tests']['SSH-7408'] == true ){
+        $::lynis::settings['tests']['SSH-7408_enabled_tests'].each |$value, $key| {
+          notice("${value} = ${key}")
         }
       }
     }
--- lynis/manifests/params.pp
+++ lynis/manifests/params.pp
@@ -6,10 +6,27 @@
 class lynis::params {
   $settings_all  =  { 'cron_enabled'  => true,
                       'cron_email_to' => "root",
-                      'tests'         =>  { 'AUTH-9328' =>  true,
-                                            'FILE-6310' =>  true,
-                                            'STRG-1840' =>  true,
-                                            'STRG-1846' =>  true
+                      'tests'         =>  { 'AUTH-9328'               =>  true,
+                                            'FILE-6310'               =>  true,
+                                            'STRG-1840'               =>  true,
+                                            'STRG-1846'               =>  true,
+                                            'NETW-3032'               =>  true,
+                                            'SSH-7408'                =>  true,
+                                            'SSH-7408_enabled_tests'  => {  'UseDNS'                =>  'yes',
+                                                                            'X11Forwarding'         =>  'no',
+                                                                            'AllowAgentForwarding'  =>  'no',
+                                                                            'AllowTcpForwarding'    =>  'no'
+                                                                         },
+                                            'SSH-7408_disabled_tests' => ['ClientAliveCountMax',
+                                                                          'Compression',
+                                                                          'LogLevel',
+                                                                          'MaxAuthTries',
+                                                                          'MaxSessions',
+                                                                          'PermitRootLogin',
+                                                                          'Port',
+                                                                          'TCPKeepAlive',
+                                                                          'UseDNS'
+                                                                         ],
                       }
                     }
   case $::osfamily {
