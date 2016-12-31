59f3e727ef1ea07e13c58e4424452a42d61a6196
Karim Elatov <elatov@users.noreply.github.com>
2016-12-30 17:01:15 -0700

    finish up removing augeas providers

--- dis_ipv6/manifests/init.pp
+++ dis_ipv6/manifests/init.pp
@@ -14,31 +14,6 @@ class dis_ipv6 {
 	case $::operatingsystem {
 	    /(?i:CentOS)/: { 
 	      if ($::operatingsystemmajrelease == '7') { 
-#	        class { 'augeasproviders::instances':
-#	          sysctl_hash => { 'net.ipv6.conf.all.disable_ipv6' => { 
-#	                            'value' => '1',
-#	                             'target' => "/etc/sysctl.d/90-dis_ipv6.conf",
-#	                         },
-#	                         'net.ipv6.conf.default.disable_ipv6' => { 
-#	                            'value' => '1',
-#	                             'target' => "/etc/sysctl.d/90-dis_ipv6.conf",  
-#	                         },
-#	                         'net.core.rmem_max' => { 
-#                              'value' => '4194304',
-#                               'target' => "/etc/sysctl.d/91-perf.conf",  
-#                           },
-#                           'net.core.wmem_max' => { 
-#                              'value' => '1048576',
-#                               'target' => "/etc/sysctl.d/91-perf.conf",  
-#                           },
-#	         },
-#	         sshd_config_hash => { 'AddressFamily' => { 
-#                              'ensure' => 'present',
-#                               'value' => "inet",
-#                               notify => Service["sshd"],
-#                               },
-#           },
-#	        }
 					augeas { "sysctl_conf-net.ipv6.conf.all.disable_ipv6":
 					  incl    => "/etc/sysctl.d/90-dis_ipv6.conf",
 						context => "/files/etc/sysctl.d/90-dis_ipv6.conf",
@@ -74,12 +49,6 @@ class dis_ipv6 {
             changes => "set net.core.wmem_max '1048576'",
             notify  => Exec["sysctl"],
           }
-          
-					exec { "sysctl -p":
-						alias       => "sysctl",
-						refreshonly => true,
-						path    => ['/usr/bin', '/usr/sbin',],
-					}
 					
 					augeas { "sshd_config-ipv6":
 						context => "/files/etc/ssh/sshd_config",
@@ -88,11 +57,7 @@ class dis_ipv6 {
 						            ],
 						notify => Service["sshd"],
 					}
-#	        augeas { "main_cf_config":
-#            context => "/files/etc/postfix/main.cf",
-#            changes => ["set inet_protocols ipv4",],
-#            notify => Service["postfix"],
-#          }
+
           augeas {'disable_ipv6_host_entry':
 	          incl    => "/etc/hosts",
 	          lens    => "Hosts.lns",
@@ -101,66 +66,89 @@ class dis_ipv6 {
 	          onlyif  => "match *[ipaddr = '::1'] size >= 1"
           }
 	      } else { 
-	        class { 'augeasproviders::instances':
-	          sysctl_hash => { 'net.ipv6.conf.all.disable_ipv6' => { 
-	                            'value' => '1',
-	                         },
-	                         'net.ipv6.conf.default.disable_ipv6' => { 
-	                            'value' => '1',
-	                         },
-	         },
-	         sshd_config_hash => { 'AddressFamily' => { 
-                              'ensure' => 'present',
-                               'value' => "inet",
-                               notify => Service["sshd"],
-                               },
-           }, 
-	        }
-#	        augeas { "main_cf_config":
-#            context => "/files/etc/postfix/main.cf",
-#            changes => ["set inet_protocols ipv4",],
-#            notify => Service["postfix"],
-#          }
+#	        class { 'augeasproviders::instances':
+#	          sysctl_hash => { 'net.ipv6.conf.all.disable_ipv6' => { 
+#	                            'value' => '1',
+#	                         },
+#	                         'net.ipv6.conf.default.disable_ipv6' => { 
+#	                            'value' => '1',
+#	                         },
+#	         },
+#	         sshd_config_hash => { 'AddressFamily' => { 
+#                              'ensure' => 'present',
+#                               'value' => "inet",
+#                               notify => Service["sshd"],
+#                               },
+#           }, 
+#	        }
+
+          augeas { "sshd_config-ipv6":
+            context => "/files/etc/ssh/sshd_config",
+            changes =>  [
+                        "set AddressFamily inet",
+                        ],
+            notify => Service["sshd"],
+          }
 	      }
 	    }
 	    /(?i:fedora)/: { 
-	      class { 'augeasproviders::instances':
-	          sysctl_hash => { 'net.ipv6.conf.all.disable_ipv6' => { 
-	                            'value' => '1',
-	                             'target' => "/etc/sysctl.d/90-dis_ipv6.conf",
-	                         },
-	                         'net.ipv6.conf.default.disable_ipv6' => { 
-	                            'value' => '1',
-	                             'target' => "/etc/sysctl.d/90-dis_ipv6.conf",  
-	                         },
-	         },
-	         sshd_config_hash => { 'AddressFamily' => { 
-                              'ensure' => 'present',
-                               'value' => "inet",
-                               notify => Service["sshd"],
-                               },
-           }, 
-	        }
-#	        augeas { "main_cf_config":
-#            context => "/files/etc/postfix/main.cf",
-#            changes => ["set inet_protocols ipv4",],
-#            notify => Service["postfix"],
-#          }
+        augeas { "sysctl_conf-net.ipv6.conf.all.disable_ipv6":
+            incl    => "/etc/sysctl.d/90-dis_ipv6.conf",
+            context => "/files/etc/sysctl.d/90-dis_ipv6.conf",
+            lens    => "Simplevars.lns",
+            onlyif  => "get net.ipv6.conf.all.disable_ipv6 != '1'",
+            changes => "set net.ipv6.conf.all.disable_ipv6 '1'",
+            notify  => Exec["sysctl"],
+          }
+          
+          augeas { "sysctl_conf-net.ipv6.conf.default.disable_ipv6":
+            incl    => "/etc/sysctl.d/90-dis_ipv6.conf",
+            context => "/files/etc/sysctl.d/90-dis_ipv6.conf",
+            lens    => "Simplevars.lns",
+            onlyif  => "get net.ipv6.conf.default.disable_ipv6 != '1'",
+            changes => "set net.ipv6.conf.default.disable_ipv6 '1'",
+            notify  => Exec["sysctl"],
+          }
+          
+          augeas { "sshd_config-ipv6":
+            context => "/files/etc/ssh/sshd_config",
+            changes =>  [
+                        "set AddressFamily inet",
+                        ],
+            notify => Service["sshd"],
+          }
 	    }
 	    /(?i:Debian|Ubuntu)/: { 
-	      class { 'augeasproviders::instances':
-	         sysctl_hash       => { 'net.ipv6.conf.all.disable_ipv6' => { 
-	                                'value'   => '1',
-	                                'target'  => "/etc/sysctl.d/90-dis_ipv6.conf",
-	                              }
-	         },
-	         sshd_config_hash  => { 'AddressFamily' => { 
-                                 'ensure' => 'present',
-                                 'value'  => "inet",
-                                 'notify' => Service["sshd"],
-                                },
-           },   
-	      }
+#	      class { 'augeasproviders::instances':
+#	         sysctl_hash       => { 'net.ipv6.conf.all.disable_ipv6' => { 
+#	                                'value'   => '1',
+#	                                'target'  => "/etc/sysctl.d/90-dis_ipv6.conf",
+#	                              }
+#	         },
+#	         sshd_config_hash  => { 'AddressFamily' => { 
+#                                 'ensure' => 'present',
+#                                 'value'  => "inet",
+#                                 'notify' => Service["sshd"],
+#                                },
+#           },   
+#	      }
+        augeas { "sysctl_conf-net.ipv6.conf.all.disable_ipv6":
+            incl    => "/etc/sysctl.d/90-dis_ipv6.conf",
+            context => "/files/etc/sysctl.d/90-dis_ipv6.conf",
+            lens    => "Simplevars.lns",
+            onlyif  => "get net.ipv6.conf.all.disable_ipv6 != '1'",
+            changes => "set net.ipv6.conf.all.disable_ipv6 '1'",
+            notify  => Exec["sysctl"],
+        }
+        
+        augeas { "sshd_config-ipv6":
+            context => "/files/etc/ssh/sshd_config",
+            changes =>  [
+                        "set AddressFamily inet",
+                        ],
+            notify => Service["sshd"],
+          }
+          
 				kmod::blacklist { 'ipv6': }
 				augeas {'disable_ipv6_netconfig_udp':
 				  incl    => "/etc/netconfig",
@@ -197,14 +185,13 @@ class dis_ipv6 {
           changes => "rm *[ipaddr = '::1']",
           onlyif  => "match *[ipaddr = '::1'] size >= 1"
         }
-	      class { 'augeasproviders::instances':
-           sshd_config_hash  => { 'AddressFamily' => { 
-                                 'ensure' => 'present',
-                                 'value'  => "inet",
-                                 'notify' => Service["sshd"],
-                                },
-           },   
-        }
+	      augeas { "sshd_config-ipv6":
+            context => "/files/etc/ssh/sshd_config",
+            changes =>  [
+                        "set AddressFamily inet",
+                        ],
+            notify => Service["sshd"],
+          }
         
 				file_line { "ip6addrctl_e_in_rc_conf":
 					path => '/etc/rc.conf',
@@ -227,14 +214,13 @@ class dis_ipv6 {
 				}
 	    }
 	    default: {
-	      class { 'augeasproviders::instances':
-					sshd_config_hash => { 'AddressFamily' => { 
-					                     'ensure' => 'present',
-					                     'value'  => "inet",
-					                     'notify' => Service["sshd"],
-					                     },
-					},   
-	      }
+	      augeas { "sshd_config-ipv6":
+            context => "/files/etc/ssh/sshd_config",
+            changes =>  [
+                        "set AddressFamily inet",
+                        ],
+            notify => Service["sshd"],
+        }
 	    }
 	  }
 	  
@@ -246,14 +232,9 @@ class dis_ipv6 {
     enable  => true,
 		ensure  => running,
 	}
-	
-	#service { "postfix":
-   # name => $operatingsystem ? {
-  #    /(?i:Debian|Ubuntu)/ => "postfix",
-  #    default => "postfix",
- # },
- # require => Class["augeasproviders::instances"],
- #   enable => true,
-  #  ensure => running,
- # }
+	exec { "sysctl -p":
+		alias       => "sysctl",
+		refreshonly => true,
+		path    => ['/usr/bin', '/usr/sbin',],
+	}
 }
