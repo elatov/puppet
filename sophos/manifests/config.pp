0e97bc73891f45b189ea2dac3103d1a43df7ff67
Karim Elatov <elatov@users.noreply.github.com>
2017-01-02 12:23:05 -0700

    vg

--- sophos/manifests/config.pp
+++ sophos/manifests/config.pp
@@ -12,7 +12,7 @@ class sophos::config {
 	      if ( $::sophos::settings['enable_liveprotection'] == true ){
 					exec { "${module_name}-enable_livep":
 					        cwd    =>  "${::sophos::install_dir}/bin",
-									command => './savconfig set LiveProtection true',
+									command => "${::sophos::install_dir}/bin/savconfig set LiveProtection true",
 									unless  => "${::sophos::install_dir}/bin/savconfig query LiveProtection | /bin/grep true"
 								} 
 	      }
