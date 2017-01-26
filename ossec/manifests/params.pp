2001ae859e9e32ae4bd68ada57a2d1d4da67a4ea
Karim Elatov <elatov@users.noreply.github.com>
2017-01-26 11:43:17 -0700

    removing the default time-zone file since we don't use it any more

--- ossec/manifests/params.pp
+++ ossec/manifests/params.pp
@@ -59,7 +59,16 @@ class ossec::params {
 			## Conf Files
       $ossec_client_config_file     = "ossec-agent.conf"
 			## Settings
-			$ossec_client_settings_os     = { 'timezone_file' => '/usr/share/zoneinfo/America/Denver',
+#			$ossec_client_settings_os     = { 'timezone_file' => '/usr/share/zoneinfo/America/Denver',
+#                                        'add_user'      => 'test',
+#                                        'config'        => { 'logs' => { '/var/log/messages'       => 'syslog',
+#                                                                         '/var/log/secure'         => 'syslog',
+#                                                                         '/var/log/maillog'        => 'syslog',
+#                                                                         '/var/log/exim/main.log'  => 'syslog',
+#                                                                        }
+#                                                           }
+#                                      }
+      $ossec_client_settings_os     = { 
                                         'add_user'      => 'test',
                                         'config'        => { 'logs' => { '/var/log/messages'       => 'syslog',
                                                                          '/var/log/secure'         => 'syslog',
