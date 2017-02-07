51e7efc0e0782915f7953c2b32da04317c533a07
Karim Elatov <elatov@users.noreply.github.com>
2017-02-07 15:21:48 -0700

    update aureport script to exclude a command on debian

--- lynis/files/audit-reports.sh
+++ lynis/files/audit-reports.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+OS=$(uname -s)
+
 /sbin/aureport -au
 /sbin/aureport -l
-/sbin/aureport -x --summary | /bin/head -20 
-/sbin/aureport -f --summary | /bin/head -20
+if [ ${OS} = "Linux" ]; then
+     if [ -f /etc/redhat-release ]; then
+        /sbin/aureport -x --summary | /usr/bin/head -20 
+     elif [ -f /etc/debian_version ]; then
+        # do nothing
+        # due to this bug https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=841272
+    fi
+elif [ ${OS} = 'FreeBSD' ]; then
+    # do nothing
+fi
+
+/sbin/aureport -f --summary | /usr/bin/head -20
 /sbin/aureport -u -i --summary
 /sbin/aureport --failed
