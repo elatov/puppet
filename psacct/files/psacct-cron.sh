b6d565648c61d22f0750fd7213e5a05a40cc9d62
Karim Elatov <elatov@users.noreply.github.com>
2017-02-12 09:53:07 -0700

    get creative to replace head -2 for freebsd

--- psacct/files/psacct-cron.sh
+++ psacct/files/psacct-cron.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 OS=$(uname -s)
 if [ ${OS} = "Linux" ]; then
+# setup variables
+
      if [ -f /etc/redhat-release ]; then
         AC=/bin/ac
         LAST=/bin/last
@@ -20,7 +22,25 @@ if [ ${OS} = "Linux" ]; then
         HEAD=/usr/bin/head
         ECHO=/bin/echo
     fi
+
+# run the actual commands
+
+    $ECHO -e "User stats\n"
+	$AC -p
+	$ECHO -e "\n"
+	$ECHO -e "elatov commands\n"
+	$LASTCOMM elatov | $AWK '{print $1}' | $SORT | $UNIQ -c  | $SORT -nr | $HEAD
+	$ECHO -e "\n"
+	$ECHO -e "User Logins\n"
+	$LAST | $HEAD -n -2| $AWK '{print $1}' | $SORT | $UNIQ -c  | $SORT -nr | $HEAD
+	$ECHO -e "\n"
+	$ECHO -e "Host Logins\n"
+	$LAST | $HEAD -n -2 | $AWK '{print $3}' | $SORT | $UNIQ -c  | $SORT -nr | $HEAD
+	$ECHO -e "\n"
+	
 elif [ ${OS} = 'FreeBSD' ]; then
+# setup variables
+
 	AC=/usr/bin/ac
 	LAST=/usr/bin/last
 	LASTCOMM=/usr/bin/lastcomm
@@ -29,16 +49,20 @@ elif [ ${OS} = 'FreeBSD' ]; then
 	UNIQ=/usr/bin/uniq
 	HEAD=/usr/bin/head
 	ECHO=/bin/echo
+	TAIL=/usr/bin/tail
+	SED=/usr/bin/sed
+
+# run the actual commands
+	
+	$ECHO "User stats\n"
+	$AC -p
+	$ECHO "\n"
+	$ECHO "elatov commands\n"
+	$LASTCOMM elatov | $AWK '{print $1}' | $SORT | $UNIQ -c  | $SORT -nr | $HEAD
+	$ECHO "\n"
+	$ECHO "User Logins\n"
+	$LAST | $TAIL -r | $sed '1,2d'|$TAIL -r | $AWK '{print $1}' | $SORT | $UNIQ -c  | $SORT -nr | $HEAD
+	$ECHO "\n"
+	$ECHO "Host Logins\n"
+	$LAST | $TAIL -r | $sed '1,2d'|$TAIL -r | $AWK '{print $3}' | $SORT | $UNIQ -c  | $SORT -nr | $HEAD
 fi
-$ECHO -e "User stats\n"
-$AC -p
-$ECHO -e "\n"
-$ECHO -e "elatov commands\n"
-$LASTCOMM elatov | $AWK '{print $1}' | $SORT | $UNIQ -c  | $SORT -nr | $HEAD
-$ECHO -e "\n"
-$ECHO -e "User Logins\n"
-$LAST | $HEAD -n -2| $AWK '{print $1}' | $SORT | $UNIQ -c  | $SORT -nr | $HEAD
-$ECHO -e "\n"
-$ECHO -e "Host Logins\n"
-$LAST | $HEAD -n -2 | $AWK '{print $3}' | $SORT | $UNIQ -c  | $SORT -nr | $HEAD
-$ECHO -e "\n"
