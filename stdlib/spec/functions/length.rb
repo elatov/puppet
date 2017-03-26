a9fd5f96fb191434dc09dfc4c9ca5484c2dffbff
Karim Elatov <elatov@gmail.com>
2017-03-26 08:21:47 -0600

    update stdlib and remove postgres

--- stdlib/spec/functions/length.rb
+++ stdlib/spec/functions/length.rb
@@ -1,16 +1,13 @@
 require 'spec_helper'
 
-describe 'size' do
+describe 'length' do
   it { is_expected.not_to eq(nil) }
-  it { is_expected.to run.with_params().and_raise_error(Puppet::ParseError, /wrong number of arguments/i) }
-  it {
-    pending("Current implementation ignores parameters after the first.")
-    is_expected.to run.with_params([], 'extra').and_raise_error(Puppet::ParseError, /wrong number of arguments/i)
-  }
-  it { is_expected.to run.with_params(1).and_raise_error(Puppet::ParseError, /Unknown type given/) }
-  it { is_expected.to run.with_params(true).and_raise_error(Puppet::ParseError, /Unknown type given/) }
-  it { is_expected.to run.with_params('1').and_raise_error(Puppet::ParseError, /Requires either string, array or hash to work/) }
-  it { is_expected.to run.with_params('1.0').and_raise_error(Puppet::ParseError, /Requires either string, array or hash to work/) }
+  it { is_expected.to run.with_params().and_raise_error(ArgumentError, /'length' expects 1 argument, got none/) }
+  it { is_expected.to run.with_params([], 'extra').and_raise_error(ArgumentError, /'length' expects 1 argument, got 2/) }
+  it { is_expected.to run.with_params(1).and_raise_error(ArgumentError, /expects a value of type String, Array, or Hash, got Integer/) }
+  it { is_expected.to run.with_params(true).and_raise_error(ArgumentError, /expects a value of type String, Array, or Hash, got Boolean/) }
+  it { is_expected.to run.with_params('1').and_return(1) }
+  it { is_expected.to run.with_params('1.0').and_return(3) }
   it { is_expected.to run.with_params([]).and_return(0) }
   it { is_expected.to run.with_params(['a']).and_return(1) }
   it { is_expected.to run.with_params(['one', 'two', 'three']).and_return(3) }
@@ -31,7 +28,7 @@ describe 'size' do
   context 'when using a class extending String' do
     it 'should call its size method' do
       value = AlsoString.new('asdfghjkl')
-      value.expects(:size).returns('foo')
+      value.expects(:length).returns('foo')
       expect(subject).to run.with_params(value).and_return('foo')
     end
   end
