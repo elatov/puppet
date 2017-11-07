270ae5443d529e19e70b2525bd7ef8fde8c67f98
Karim Elatov <elatov@users.noreply.github.com>
2017-11-07 09:50:57 -0700

    try aureport with debian 9

--- lynis/files/audit-reports.sh
+++ lynis/files/audit-reports.sh
@@ -9,9 +9,9 @@ if [ ${OS} = "Linux" ]; then
         /sbin/aureport -f --summary | /usr/bin/head -20
         /sbin/aureport -x --summary | /usr/bin/head -20 
      elif [ -f /etc/debian_version ]; then
-     	W="nothing"
-        # do nothing
         # due to this bug https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=841272
+        /sbin/aureport -f --summary | /usr/bin/head -20
+        /sbin/aureport -x --summary | /usr/bin/head -20
     fi
 elif [ ${OS} = 'FreeBSD' ]; then
 	W="nothing"
