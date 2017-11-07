de24b910b9873933f76a5ec16bce8c0ed778a618
Karim Elatov <elatov@users.noreply.github.com>
2017-11-07 11:03:35 -0700

    Don't user omniti repo since it's offline

--- smartd/manifests/install.pp
+++ smartd/manifests/install.pp
@@ -3,12 +3,12 @@
 class smartd::install {
 
   if ($::operatingsystem == 'OmniOS') {
-		pkg_publisher { 'ms.omniti.com':
-			origin  => 'http://pkg.omniti.com/omniti-ms',
+		pkg_publisher { 'cs.umd.edu':
+			origin  => 'http://pkg.cs.umd.edu/',
 			enable  => true,
 			ensure  => 'present',
 		}
-    ensure_packages ($smartd::package_name,{ 'ensure'=> 'present', require => Pkg_publisher['ms.omniti.com'] })
+    ensure_packages ($smartd::package_name,{ 'ensure'=> 'present', require => Pkg_publisher['cs.umd.edu'] })
   } else {
     ensure_packages ($smartd::package_name,{ 'ensure'=> 'present' })
   }
