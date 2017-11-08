1f1773748764564d442a9f370eb38f1bac6f2e4f
Karim Elatov <elatov@users.noreply.github.com>
2017-11-08 11:28:12 -0700

    mimic aureport for debian

--- lynis/files/audit-reports.sh
+++ lynis/files/audit-reports.sh
@@ -10,6 +10,7 @@ if [ ${OS} = "Linux" ]; then
         /sbin/aureport -x --summary | /usr/bin/head -20 
      elif [ -f /etc/debian_version ]; then
         # due to this bug https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=841272
+        /sbin/aureport -l
         /sbin/aureport -f --summary | /usr/bin/head -20
         /sbin/aureport -x --summary | /usr/bin/head -20
     fi
