df8872d4aea7427184574a568b13333514bf7530
elatov <elatov@users.noreply.github.com>
2017-11-21 21:35:01 -0700

    keep updating ohmyzsh template

--- ohmyzsh/manifests/config.pp
+++ ohmyzsh/manifests/config.pp
@@ -11,11 +11,8 @@ class ohmyzsh::config {
                     )
     exec { "ohmyzsh copy theme for ${ohmyzsh::settings['user']}":
       path  => ['/bin','/usr/bin/'],
-      onlyif  => "test -d ${ohmyzsh::user_home_dir}/
-      .oh-my-zsh/custom/themes/${ohmyzsh::settings['install_theme']}",
-      command => "cp -rp ${ohmyzsh::user_home_dir}/
-      .oh-my-zsh/themes/${ohmyzsh::settings['install_theme']} ${ohmyzsh::user_home_dir}/
-      .oh-my-zsh/custom/themes/${ohmyzsh::settings['install_theme']}",
+      onlyif  => "test -d ${ohmyzsh::user_home_dir}/.oh-my-zsh/custom/themes/${ohmyzsh::settings['install_theme']}",
+      command => "cp -rp ${ohmyzsh::user_home_dir}/.oh-my-zsh/themes/${ohmyzsh::settings['install_theme']} ${ohmyzsh::user_home_dir}/.oh-my-zsh/custom/themes/${ohmyzsh::settings['install_theme']}",
       require => Vcsrepo["${ohmyzsh::user_home_dir}/.oh-my-zsh"]
     }
   }
