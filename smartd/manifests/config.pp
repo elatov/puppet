b3827067ca5e84476f74babb3fe67a164c237024
elatov <elatov@users.noreply.github.com>
2017-11-23 16:35:49 -0700

    update exim config to support debian as clientˆ

--- smartd/manifests/config.pp
+++ smartd/manifests/config.pp
@@ -4,53 +4,56 @@
 #
 class smartd::config {
 
-  if $smartd::service_file =~ /(?i:.service)/ {
-    file { $smartd::service_file:
-      ensure  => "present",
-      path    => "${smartd::service_dir}/${smartd::service_file}",
-      mode    => '0644',
-      content => template("smartd/${smartd::service_file}.erb"),
-    }~>
-    exec { "${module_name}-reload-systemd":
-      path    		=> ["/bin","/usr/bin"],
-      command 		=> "systemctl daemon-reload",
-      refreshonly => true,
+  if ($smartd::service_file != undef) {
+
+    if $smartd::service_file =~ /(?i:.service)/ {
+      file { $smartd::service_file:
+        ensure  => "present",
+        path    => "${smartd::service_dir}/${smartd::service_file}",
+        mode    => '0644',
+        content => template("smartd/${smartd::service_file}.erb"),
+      } ~>
+      exec { "${module_name}-reload-systemd":
+        path        => ["/bin", "/usr/bin"],
+        command     => "systemctl daemon-reload",
+        refreshonly => true,
+      }
+    }
+
+    if $smartd::service_file =~ /(?i:.init)/ {
+      file { $smartd::service_file:
+        ensure => 'present',
+        path   => "${smartd::service_dir}/smartd",
+        mode   => '0755',
+        source => "puppet:///modules/smartd/${smartd::service_file}",
+      }
+    }
+
+    if $smartd::service_file =~ /(?i:.smf)/ {
+      file { $smartd::service_file:
+        ensure => "present",
+        path   => "${smartd::service_dir}/svc-smartd",
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'bin',
+        source => "puppet:///modules/smartd/${smartd::service_file}",
+      } ->
+      file { $smartd::manifest_file:
+        ensure => "present",
+        path   => "${smartd::manifest_dir}/${smartd::manifest_file}",
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'sys',
+        source => "puppet:///modules/smartd/${smartd::manifest_file}",
+      } ~>
+      exec { "${module_name}-import-svc":
+        path        => ["/sbin", "/usr/sbin"],
+        command     => "svccfg import ${smartd::manifest_dir}/${
+          smartd::manifest_file}",
+        refreshonly => true,
+      }
     }
   }
-  
-  if $smartd::service_file =~ /(?i:.init)/ {
-    file { $smartd::service_file:
-      ensure  => 'present',
-      path    => "${smartd::service_dir}/smartd",
-      mode    => '0755',
-      source => "puppet:///modules/smartd/${smartd::service_file}",
-    }
-  }
-  
-  if $smartd::service_file =~ /(?i:.smf)/ {
-    file { $smartd::service_file:
-      ensure  => "present",
-      path    => "${smartd::service_dir}/svc-smartd",
-      mode    => '0555',
-      owner   => 'root',
-      group   => 'bin',
-      source => "puppet:///modules/smartd/${smartd::service_file}",
-    }->
-    file { $smartd::manifest_file: 
-      ensure  => "present",
-      path    => "${smartd::manifest_dir}/${smartd::manifest_file}",
-      mode    => '0444',
-      owner   => 'root',
-      group   => 'sys',
-      source => "puppet:///modules/smartd/${smartd::manifest_file}",
-    }~>
-    exec { "${module_name}-import-svc":
-      path        => ["/sbin","/usr/sbin"],
-      command     => "svccfg import ${smartd::manifest_dir}/${smartd::manifest_file}",
-      refreshonly => true,
-    }
-  }
-  
   file { $smartd::config_dir:
     ensure  => 'directory',
   }
