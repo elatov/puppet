4a8f7c3eca977d7c61db84c24a25d2cf20430e32
Karim Elatov <elatov@users.noreply.github.com>
2019-04-13 22:21:55 -0600

    update config to ignore promisc warning when using arpwatch

--- lynis/manifests/config.pp
+++ lynis/manifests/config.pp
@@ -355,11 +355,12 @@ class lynis::config {
       
       if ( $::lynis::settings['tests']['NETW-3032'] == true ){
         class {'arpwatch': }
-		file_line{"add-whitelist-promisc":
-			path => "${::lynis::conf_dir}/${::lynis::conf_file}",
-			#line => "if_promisc:$facts['networking']['interfaces']",
-			line => "if_promisc:ens224",
-		}
+        file_line{"add-whitelist-promisc":
+          path => "${::lynis::conf_dir}/${::lynis::conf_file}",
+          #line => "if_promisc:$facts['networking']['interfaces']",
+          line => "if_promisc:${facts['networking']['primary']}"
+          # line => "if_promisc:ens224",
+        }
       }
       
       if ( $::lynis::settings['tests']['SSH-7408'] == true ){
